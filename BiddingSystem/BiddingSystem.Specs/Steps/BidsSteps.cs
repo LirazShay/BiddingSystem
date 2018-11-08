ec1d5f0220971fb68b1ab77ddaceca9e089aacd6
Liraz Shay <liraz@om2.com>
2018-11-08 16:59:35 +0200

    create BidsSteps

---
+++ BiddingSystem/BiddingSystem.Specs/Steps/BidsSteps.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechTalk.SpecFlow;
+
+namespace BiddingSystem.Specs.Steps
+{
+    public class BidsSteps : StepsBase
+    {
+        [When(@"I place a bid")]
+        public void WhenIPlaceABid(Table table)
+        {
+        }
+
+    }
+}
