8853b3da3e5ac254999636e9edba1edd6005dced
Liraz Shay <liraz@om2.com>
2018-11-18 12:32:25 +0200

    added 2 tests

---
+++ BiddingSystem/BiddingSystem.Services.Tests/BidsServiceTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+using NUnit.Framework;
+using BiddingSystem.Entities;
+using KellermanSoftware.CompareNetObjects;
+
+namespace BiddingSystem.Services.Tests
+{
+    [TestFixture]
+    public class BidsServiceTests
+    {
+        [Test]
+        public void PlaceBid_WithValidUsernameAndPrice_WillBeSaved()
+        {
+            var newBid = new Bid()
+            {
+                Price = 10,
+                Username = "Vadim"
+            };
+
+            BidsService.PlaceBid(newBid);
+
+            var bids = BidsService.GetAllBids();
+            var actualBid = bids.First();
+            actualBid.ShouldCompare(newBid);
+        }
+
+        [Test]
+        public void PlaceBid_WithNullBid_WillThrowException()
+        {
+            TestDelegate act = 
+                () => BidsService.PlaceBid(null);
+
+            var ex = Assert.Throws<ArgumentNullException>(act); 
+
+            StringAssert.Contains("Bid can't be null",ex.Message);
+        }
+
+    }
+}
--- BiddingSystem/BiddingSystem.Services/BidsService.cs
+++ BiddingSystem/BiddingSystem.Services/BidsService.cs
@@ -1,13 +1,21 @@
 ﻿using BiddingSystem.Entities;
 using System;
+using System.Collections.Generic;
 
 namespace BiddingSystem.Services
 {
     public class BidsService
     {
+        static List<Bid> bids = new List<Bid>();
+
         public static void PlaceBid(Bid bid)
         {
-            throw new NotImplementedException();
+            bids.Add(bid);
+        }
+
+        public static IList<Bid> GetAllBids()
+        {
+            return bids.AsReadOnly();
         }
     }
 }
