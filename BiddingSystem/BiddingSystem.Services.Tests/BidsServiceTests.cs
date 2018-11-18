35e8f6109439e335a6d3a969405e2806617939e1
Liraz Shay <liraz@om2.com>
2018-11-18 12:50:37 +0200

    added 2 tests. but now 2 are failing if running together

--- BiddingSystem/BiddingSystem.Services.Tests/BidsServiceTests.cs
+++ BiddingSystem/BiddingSystem.Services.Tests/BidsServiceTests.cs
@@ -9,13 +9,15 @@ namespace BiddingSystem.Services.Tests
     [TestFixture]
     public class BidsServiceTests
     {
-        [Test]
-        public void PlaceBid_WithValidUsernameAndPrice_WillBeSaved()
+
+        [TestCase(0.01, "M")]
+        [TestCase(10,"Rami")]
+        public void PlaceBid_WithValidUsernameAndPrice_WillBeSaved(double price,string username)
         {
             var newBid = new Bid()
             {
-                Price = 10,
-                Username = "Vadim"
+                Price = price,
+                Username = username
             };
 
             BidsService.PlaceBid(newBid);
@@ -34,6 +36,42 @@ namespace BiddingSystem.Services.Tests
             var ex = Assert.Throws<ArgumentNullException>(act); 
 
             StringAssert.Contains("Value cannot be null.\r\nParameter name: bid",ex.Message);
+        }
+
+        [Test]
+        public void PlaceBid_WithoutPrice_WillThrowException()
+        {
+            Bid bid = new Bid() {Username = "Rami"};
+
+            TestDelegate act = () => BidsService.PlaceBid(bid);
+
+            var ex = Assert.Throws<ArgumentException>(act);
+            StringAssert.Contains("Price can't be null", ex.Message);
+        }
+
+        [Test]
+        public void PlaceBid_WithoutUsername_WillThrowException()
+        {
+            Bid bid = new Bid() { Price = 10 };
+
+            TestDelegate act = () => BidsService.PlaceBid(bid);
+
+            var ex = Assert.Throws<ArgumentException>(act);
+            StringAssert.Contains("Username can't be null", ex.Message);
+        }
+
+
+        [TestCase(-0.01)]
+        [TestCase(-1)]
+        [TestCase(0)]
+        public void PlaceBid_WithInvalidPrice_WillThrowException(double price)
+        {
+            Bid bid = new Bid() { Username = "Rami", Price = price};
+
+            TestDelegate act = () => BidsService.PlaceBid(bid);
+
+            var ex = Assert.Throws<ArgumentException>(act);
+            StringAssert.Contains("Invalid price, price should be greater than 0", ex.Message);
         }
 
     }
